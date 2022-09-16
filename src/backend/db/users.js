4e365fa48b90e5cb6b666b25eb1eb28965d8145c
yashraj2312 <yashrajsrivastava2312@gmail.com>
2022-09-16 09:39:32 +0530

    feat:modified auth details

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarsh123",
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com",
+    password: "password123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
